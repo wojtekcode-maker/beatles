39fef675857b93c03f4ced4a07b8332841c517a7
Wojciech Domżał <wojciech.domzal03@gmail.com>
2023-02-27 23:28:23 +0100

    Some content-type problems, but app is fine.

---
+++ db/db.js
@@ -0,0 +1,12 @@
+export const songs = [
+    {
+        id: "1",
+        title: "I'm Only Sleeping",
+        lyrics: `When I wake up early in the morning, I lift my head, I'm still yawning. When I'm in the middle of a dream, I stay in bed, float upstream. Please don't wake me, No, don't shake me, Leave me where I am, I'm only sleeping. Everybody seems to think I'm lazy, I don't mind, I think they're crazy, Running everywhere at such a speed, Till they find there's no need. Please don't spoil my day, I'm miles away And after all I'm only sleeping. Keeping an eye on the world going by my window, Taking my time, Lying there and staring at the ceiling, Waiting for a sleepy feeling. Please don't spoil my day, I'm miles away And after all I'm only sleeping. Keeping an eye on the world going by my window, Taking my time, When I wake up early in the morning, Lift my head, I'm still yawning. When I'm in the middle of a dream, I stay in bed, float upstream. Please don't wake me, No, don't shake me, Leave me where I am I'm only sleeping.`
+    },
+    {
+        id: "2",
+        title: "And Your Bird Can Sing",
+        lyrics: `You tell me that you've got everything you want And your bird can sing But you don't get me, you don't get me You say you've seen seven wonders and your bird is green But you can't see me, you can't see me When your prized possessions start to wear you down Look in my direction, I'll be round, I'll be round When your bird is broken will it bring you down You may be awoken, I'll be round, I'll be round You tell me that you've heard every sound there is And your bird can swing But you can't hear me, you can't hear me`
+    }
+]
--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 import express from 'express';
+import * as url from "url";
+import {songs} from "./db/db.js";
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public/'))
+
+app
+    .get('/', (req, res) => {
+        res.redirect('/song')
+    })
+    .get('/song', (req, res) => {
+        const titles = songs.map(song => [song.id, song.title]);
+        res.setHeader('content-type', 'text/html');
+        res.render('homePage', {titles, song: null});
+    })
+    .get('/song/:id', (req, res) => {
+        const titles = songs.map(song => [song.id, song.title]);
+        const {id} = req.params;
+        const [song] = songs.filter(song => {
+            if(id === song.id){
+                return song;
+            }
+        })
+        res.setHeader('content-type', 'text/html');
+        res.render('homePage', {titles, song: song});
+    })
 
 app.listen(3000, () => {
     console.log('App is listening on http://localhost:3000/');
